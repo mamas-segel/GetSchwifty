29d4a4e223f13654f8c30e23aafa6240f21ef982
itay62848 <itay62848@gmail.com>
2022-07-04 19:50:56 +0300

    Inject user interaction view to game view in main file

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import GameView from "./views/game_view.js";
 import GameController from "./controllers/game_controller.js";
 import Scoreboard from "./models/scoreboard.js";
 import ScoreboardCookie from "./models/scoreboard_cookie.js";
+import UserInteractionView from "./views/user_interaction_view.js";
 
 const cookie = new ScoreboardCookie("scoreboard", 30 * 60);
 const scoreboard = new Scoreboard(5, cookie);
 
-const gameView = new GameView(new BoardView(), new ScoreboardView());
+const gameView = new GameView(new BoardView(), new ScoreboardView(), new UserInteractionView());
 const app = new GameController(gameView, scoreboard);
 
 app.startGame();
