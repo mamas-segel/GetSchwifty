5a8cfa9cde7e8365d759b4f897bb97ef5b431fb2
itay62848 <itay62848@gmail.com>
2022-07-04 17:55:45 +0300

    Improve GameController API & receive scoreboard in ctor

--- src/controllers/game_controller.js
+++ src/controllers/game_controller.js
@@ -1,37 +1,56 @@
 import Game from "../models/game.js";
 import BoardBuilder from "../models/board_builder.js";
+import Score from "../models/score.js";
 
 export default class GameController {
-    constructor(view) {
+    constructor(view, scoreboard) {
+        this.game = null;
         this.view = view;
+        this.scoreboard = scoreboard;
+
+        this.view.listenPlayAgainClicked(this.onPlayAgainClicked);
+        this.scoreboard.listenScoresChanged(this.onScoresChanged);
     }
 
-    run() {
-        let game;
-        const boardSize = this.view.getBoardSize();
-        const boardBuilder = new BoardBuilder(boardSize);
+    startGame() {
+        const boardBuilder = new BoardBuilder(this.view.getBoardSize());
 
         do {
-            game = new Game(boardBuilder.build());
-        } while (!game.isBoardValid());
+            this.game = new Game(boardBuilder.build());
+        } while (!this.game.isBoardValid());
 
-        this.view.displayTiles(game.board.values);
-        this.view.listenTileClicked(this.onTileClicked(game));
+        this.view.displayTiles(this.game.board.values);
+        this.view.listenTileClicked(this.onTileClicked);
+    }
+
+    onPlayAgainClicked = () => {
+        this.startGame();
     }
 
     /**
-     * @param {Game} game 
+     * @param {Score[]} scores 
      */
-    onTileClicked(game) {
-        return (tileValue) => {
-            if (game.tryMoveValue(tileValue)) {
-                this.view.moveTiles(tileValue);
-                if (game.isBoardSolved()) {
-                    this.view.onBoardSolved();
-                }
-            } else {
-                this.view.warnInvalidMove(tileValue);
-            }
+    onScoresChanged = (scores) => {
+        this.view.displayScores(scores);
+    }
+
+    onTileClicked = (tileValue) => {
+        if (!this.game.tryMoveValue(tileValue)) {
+            this.view.warnInvalidMove(tileValue);
+            return;
+        }
+
+        this.view.moveTiles(tileValue);
+
+        if (this.game.isBoardSolved()) {
+            this.view.onBoardSolved();
+
+            const score = new Score(
+                this.view.getPlayerName(),
+                this.game.board.rowCount,
+                this.game.startDate
+            );
+            this.scoreboard.tryAddScore(score);
         }
     }
 }
