d982b9f00fb8d83cd7cb789ad44f13075bfb987a
itay62848 <itay62848@gmail.com>
2022-07-04 17:56:22 +0300

    Change main file to match new controller

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import GameView from "./views/game_view.js";
 import GameController from "./controllers/game_controller.js";
+import Scoreboard from "./models/scoreboard.js";
 
-const app = new GameController(new GameView());
+const app = new GameController(new GameView(), new Scoreboard(5));
 
-app.run();
+app.startGame();
