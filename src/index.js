6fbfae528a13c8a149ef07d978c63bf9ea2c01e8
itay62848 <itay62848@gmail.com>
2022-07-04 19:35:47 +0300

    Add scoreboard cookie object to main file

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ScoreboardView from "./views/scoreboard_view.js";
 import GameView from "./views/game_view.js";
 import GameController from "./controllers/game_controller.js";
 import Scoreboard from "./models/scoreboard.js";
+import ScoreboardCookie from "./models/scoreboard_cookie.js";
 
-const scoreboard = new Scoreboard(5);
+const cookie = new ScoreboardCookie("scoreboard", 30 * 60);
+const scoreboard = new Scoreboard(5, cookie);
+
 const gameView = new GameView(new BoardView(), new ScoreboardView());
 const app = new GameController(gameView, scoreboard);
 
